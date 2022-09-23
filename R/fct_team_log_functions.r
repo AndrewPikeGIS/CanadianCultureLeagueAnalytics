e4f727fcec42c46fd0de33850a89e50306b2b97e
Andrew Pike <59455297+AndrewPikeGIS@users.noreply.github.com>
2022-09-22 21:32:34 -0600

    file name change

--- R/fct_team_log_functions.r
+++ R/fct_team_log_functions.r
@@ -1,3 +1,10 @@
 transform_team_log_fd <- function(file_path) {
     team_log_table <- readr::read_csv(file_path)
 }
+
+
+# testing
+
+file_path <- "data/team_logs/fuck_yeah_tom_wilson_fd_log_2018.csv"
+
+team_log_table <- readr::read_csv(file_path)
