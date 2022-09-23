92b3301d4d19ad056e1f292643829fd341f5e7ee
Andrew Pike <59455297+AndrewPikeGIS@users.noreply.github.com>
2022-09-22 21:50:54 -0600

    added pipe

--- R/fct_team_log_functions.r
+++ R/fct_team_log_functions.r
@@ -7,4 +7,26 @@ transform_team_log_fd <- function(file_path) {
 
 file_path <- "data/team_logs/fuck_yeah_tom_wilson_fd_log_2018.csv"
 
-team_log_table <- readr::read_csv(file_path)
+team_log_table <- readr::read_csv(file_path, col_names = F)
+
+for (i in seq_along(team_log_table)) {
+    if (i == 0) {
+        test <- team_log_table[1:11, ]
+        test_t <- test %>%
+            tidyr::pivot_wider(
+                names_from = X1
+            )
+    } else if (i %% 11 == 0) {
+
+    }
+}
+
+test <- team_log_table[1:11, ]
+test_t <- t(test)
+
+
+test <- team_log_table[1:11, ]
+test_t <- test %>%
+    tidyr::pivot_wider(
+        names_from = X1
+    )
