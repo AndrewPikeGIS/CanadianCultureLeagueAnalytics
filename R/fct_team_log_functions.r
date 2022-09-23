7bc4239ca93ef773284db6a4a5495e48bf3af17b
Andrew Pike <59455297+AndrewPikeGIS@users.noreply.github.com>
2022-09-22 22:03:58 -0600

    working on team log manip

--- R/fct_team_log_functions.r
+++ R/fct_team_log_functions.r
@@ -1,9 +1,7 @@
 transform_team_log_fd <- function(file_path) {
     team_log_table <- readr::read_csv(file_path)
+    # testing
 }
-
-
-# testing
 
 file_path <- "data/team_logs/fuck_yeah_tom_wilson_fd_log_2018.csv"
 
@@ -12,10 +10,14 @@ team_log_table <- readr::read_csv(file_path, col_names = F)
 for (i in seq_along(team_log_table)) {
     if (i == 0) {
         test <- team_log_table[1:11, ]
-        test_t <- test %>%
-            tidyr::pivot_wider(
-                names_from = X1
+        test_t <- t(test) %>%
+            tibble::as_tibble(
+                .name_repair = "unique"
             )
+
+        names(test_t) <- test_t[1, ]
+
+        test_t <- test_t[-1, ]
     } else if (i %% 11 == 0) {
 
     }
@@ -23,10 +25,3 @@ for (i in seq_along(team_log_table)) {
 
 test <- team_log_table[1:11, ]
 test_t <- t(test)
-
-
-test <- team_log_table[1:11, ]
-test_t <- test %>%
-    tidyr::pivot_wider(
-        names_from = X1
-    )
