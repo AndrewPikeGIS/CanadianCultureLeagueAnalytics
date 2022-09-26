5281d1726f08c26927904fe220c818718c36b273
Andrew Pike <59455297+AndrewPikeGIS@users.noreply.github.com>
2022-09-25 22:08:31 -0600

    built function to clean log table.

--- R/fct_team_log_functions.r
+++ R/fct_team_log_functions.r
@@ -1,27 +1,38 @@
 transform_team_log_fd <- function(file_path) {
-    team_log_table <- readr::read_csv(file_path)
+    team_log_table <- readr::read_csv(file_path, col_names = F)
     # testing
+
+    for (i in seq_len(nrow(team_log_table))) {
+        if (i == 1) {
+            team_log_header <- team_log_table[1:10, ]
+            team_log_header_t <- t(team_log_header) %>%
+                tibble::as_tibble(
+                    .name_repair = "unique"
+                )
+            name_vector <- team_log_header_t %>%
+                dplyr::slice(1) %>%
+                as.character()
+
+            names(team_log_header_t) <- name_vector
+
+            team_log_table_clean <- team_log_header_t[-1, ]
+        } else if (i %% 12 == 0) {
+            table_selection <- team_log_table[(i + 1):(i + 10), ]
+            table_selection_t <- t(table_selection) %>%
+                tibble::as_tibble(
+                    .name_repair = "unique"
+                )
+            names(table_selection_t) <- name_vector
+
+            team_log_table_clean <- dplyr::bind_rows(
+                team_log_table_clean,
+                table_selection_t
+            )
+        }
+    }
+    team_log_table_clean <- head(team_log_table_clean, -1)
+
+    return(team_log_table_clean)
 }
 
 file_path <- "data/team_logs/fuck_yeah_tom_wilson_fd_log_2018.csv"
-
-team_log_table <- readr::read_csv(file_path, col_names = F)
-
-for (i in seq_along(team_log_table)) {
-    if (i == 0) {
-        test <- team_log_table[1:11, ]
-        test_t <- t(test) %>%
-            tibble::as_tibble(
-                .name_repair = "unique"
-            )
-
-        names(test_t) <- test_t[1, ]
-
-        test_t <- test_t[-1, ]
-    } else if (i %% 11 == 0) {
-
-    }
-}
-
-test <- team_log_table[1:11, ]
-test_t <- t(test)
